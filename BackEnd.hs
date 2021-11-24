c445d644c1526ea172fd1f0c97fd11c4c81b6fcc
cccsar <15-11295@usb.ve>
2021-11-24 14:23:37 -0400

    Fixed lexer print order when invoking 'failed' special command. It now prints all errors whithin the same file in line order and if there are several files, it is sorted lexicographically by file name

--- BackEnd.hs
+++ BackEnd.hs
@@ -1,4 +1,4 @@
-module BackEnd 
+module BackEnd
 ( UserState(..)
 , numberedLines
 , lexer
@@ -25,12 +25,12 @@ import qualified Tokens as Tk
 
 
 -- | Line number, and erorr string
-type ErrorContext = (Int,String) 
+type ErrorContext = (Int,String)
 type Filename = String
 -- | A dictionary is a word and a list of acceptions for that word.
 type Dictionary a b = M.Map a [b]
 
-data UserState = UState 
+data UserState = UState
     { errorDictionary :: Dictionary Filename ErrorContext,
       nextLine :: Int,
       currentOpenFile :: Maybe Filename,
@@ -42,31 +42,31 @@ data UserState = UState
 
 -- | Generic function to append list elements in map where "values" are keys.
 insertDictionary :: Ord k => k -> a -> M.Map k [a] -> M.Map k [a]
-insertDictionary word meaning = M.insertWith (++) word [meaning]
+insertDictionary word meaning = M.insertWith (\a b-> b ++ a) word [meaning]
 
 {- | Given a string repreNewLinesenting a file, returns an association list between the line 
  - number and the line content disregarding empty or only whitespace lines.
  -}
-numberedLines :: String -> [(Int,String)] 
+numberedLines :: String -> [(Int,String)]
 numberedLines = numberLines 1
     where
         numberLines :: Int -> String -> [(Int,String)]
         numberLines _ [] = []
-        numberLines n xs 
+        numberLines n xs
             | null postNewLine = [(n,preNewLine)]
-            | otherwise = if null preNewLine || all isSpace preNewLine 
-                          then next 
-                          else (n,preNewLine) : next 
-            where 
+            | otherwise = if null preNewLine || all isSpace preNewLine
+                          then next
+                          else (n,preNewLine) : next
+            where
                 (preNewLine,postNewLine) = span (/='\n') xs
                 next = numberLines (n+1) (tail postNewLine)
 
 -- | For now, optional
 checkIntegerOverflow :: Int -> Bool
-checkIntegerOverflow n 
+checkIntegerOverflow n
     | n > uprBound || n < lwrBound = False
     | otherwise                    = True
-    where 
+    where
         uprBound = 2147483647  -- Positive integer bound, equivalent to 2^31 - 1 (ommits sign bit)
         lwrBound = -2147483648 -- Negative integer bound, equivalent to 2^31 (ommits sign bit)
 
@@ -80,16 +80,16 @@ lexer = L.alexScanTokens
  - input stream of Tokens
  -}
 parse :: [Tk.ContextToken] -> Either String A.Program
-parse tks = case P.parse tks of 
+parse tks = case P.parse tks of
     Err.Ok result  -> Right result
-    Err.Failed err -> Left err 
+    Err.Failed err -> Left err
 
 
 {- Constants -}
 
 baseUserState :: UserState
-baseUserState = UState 
-                    { errorDictionary = M.empty 
+baseUserState = UState
+                    { errorDictionary = M.empty
                     , nextLine = 1
                     , currentOpenFile = Nothing
                     , pathName = ""
